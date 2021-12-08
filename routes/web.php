8b925f78ec801253f0d764458d04859063e79644
Aji Prastiya <aji.prastiya.ok@gmail.com>
2021-12-08 22:41:07 +0700

    add permission to routes

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,11 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('maps');
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
-Route::get('/inventaris_kib_a', [App\Http\Controllers\HomeController::class, 'inventaris_kib_a'])->name('inventaris_kib_a');
-Route::get('/datadasarbmd', [App\Http\Controllers\HomeController::class, 'datadasarbmd'])->name('inventaris_kib_a');
+Route::get('/inventaris_kib_a', [App\Http\Controllers\HomeController::class, 'inventaris_kib_a'])->name('inventaris_kib_a')->can('data aset.inventaris kib a');
+Route::get('/datadasarbmd', [App\Http\Controllers\HomeController::class, 'datadasarbmd'])->name('inventaris_kib_a')->can('dasar bmd.index');
+Route::get('admin/roles', [App\Http\Controllers\RoleController::class, 'index'])->name('roles.index');
+Route::get('admin/roles/create', [App\Http\Controllers\RoleController::class, 'create'])->name('roles.create');
+Route::post('admin/roles', [App\Http\Controllers\RoleController::class, 'store'])->name('roles.store');
+Route::post('admin/roles/{role}/edit', [App\Http\Controllers\RoleController::class, 'edit'])->name('roles.edit');
+Route::put('admin/roles/{role}', [App\Http\Controllers\RoleController::class, 'update'])->name('roles.update');
+Route::delete('admin/roles/{role}', [App\Http\Controllers\RoleController::class, 'destroy'])->name('roles.destroy');
